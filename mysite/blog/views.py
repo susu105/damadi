758147ba6ea169913d4278288a1bade2c29391ed
disuarezda <disuarezda@gmail.com>
2025-02-08 21:04:50 +0100

    View messages have been changed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    return HttpResponse("Hello, world.")
